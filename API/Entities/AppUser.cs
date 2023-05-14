a49a40a4538eaccd47d9c83559542ef1f94c45e5
Yves <yvesduke06@live.co.uk>
2023-05-14 22:56:54 +0100

    Client Skeleton

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -3,6 +3,6 @@ namespace API.Entities
     public class AppUser
     {
         public int Id { get; set; }
-        public string USerName { get; set; }
+        public string UserName { get; set; }
     }
 }
