41f500d5e10025e24659e61119913893fdd981a0
Yves <yvesduke06@live.co.uk>
2023-07-24 15:01:33 +0100

    forms for user registration

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -11,7 +11,17 @@ namespace API.DTOs
         [Required]
         public string Username { get; set; }
         [Required]
-        [StringLength(8, MinimumLength = 4)]
+        public string KnownAs { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public DateOnly? DateOfBirth { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string Country { get; set; }
+        [Required]
+        [StringLength(9, MinimumLength = 4)]
         public string Password { get; set; }
     }
 }
