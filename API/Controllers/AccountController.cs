838b84cfaecf95c906901e7292af14e2f30fa5ae
Yves <yvesduke06@live.co.uk>
2024-12-13 17:12:46 +0000

    Update to from .Net 7 to . Net8

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -13,14 +13,11 @@ namespace API.Controllers
 {
     public class AccountController : BaseApiController
     {
-        // private readonly DataContext _context;
         private readonly UserManager<AppUser> _userManager;
         private readonly ITokenService _tokenService;
         public readonly IMapper _mapper;
-        // public AccountController(DataContext context, ITokenService tokenService, IMapper mapper)
         public AccountController(UserManager<AppUser> userManager, ITokenService tokenService, IMapper mapper)
         {
-            // _context = context;
             _userManager = userManager;
             _tokenService = tokenService;
             _mapper = mapper;
@@ -33,13 +30,7 @@ namespace API.Controllers
 
             var user = _mapper.Map<AppUser>(registerDto);
 
-            // using var hmac = new HMACSHA512();
             user.UserName = registerDto.Username.ToLower();
-            // user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password));
-            // user.PasswordSalt = hmac.Key;
-
-            // _context.Users.Add(user);
-            // await _context.SaveChangesAsync();
 
             var result = await _userManager.CreateAsync(user, registerDto.Password);
 
@@ -52,7 +43,6 @@ namespace API.Controllers
             return new UserDto
             {
                 Username = user.UserName,
-                // Token = _tokenService.CreateToken(user),
                 Token = await _tokenService.CreateToken(user),
                 KnownAs = user.KnownAs,
                 Gender = user.Gender
@@ -62,29 +52,19 @@ namespace API.Controllers
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
-            // var user = await _context.Users
             var user = await _userManager.Users
                .Include(p => p.Photos)
                .SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
 
             if (user == null) return Unauthorized("izina ntiryemewe");
 
-            // using var hmac = new HMACSHA512(user.PasswordSalt);
-
-            // var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
-
             var result = await _userManager.CheckPasswordAsync(user, loginDto.Password);
 
-            // for (int i = 0; i < computedHash.Length; i++)
-            // {
-            //     if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("invalid password");
-            // }
             if (!result) return Unauthorized();
 
             return new UserDto
             {
                 Username = user.UserName,
-                // Token = _tokenService.CreateToken(user),
                 Token = await _tokenService.CreateToken(user),
                 PhotUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
                 KnownAs = user.KnownAs,
@@ -94,7 +74,6 @@ namespace API.Controllers
 
         private async Task<bool> UserExists(string username)
         {
-            // return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
             return await _userManager.Users.AnyAsync(x => x.UserName == username.ToLower());
         }
     }
